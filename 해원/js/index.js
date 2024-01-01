f0b9ef42e474de9de3a6f0c6c899962f56a55ec4
해원 <haewon1106@gmail.com>
2024-01-01 23:30:43 +0900

    Docs: 클릭 시 글자와 함께 커지는 하자 수정

--- 해원/js/index.js
+++ 해원/js/index.js
@@ -1,16 +1,22 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//     const yearBox = document.getElementsByClassName("year-box");
-//     yearBox.addEventListener('click', zoomIn);
-
-//     function zoomIn(event){
-//         yearBox.style.transform = "scale(1000)";
-//         yearBox.style.zIndex = 1;
-//         yearBox.style.transition = "all 0.5s";
-//     };
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     const yearBoxes = document.getElementsByClassName("year-box");
+
+
+    const roller = document.querySelector(".year-container");
+    roller.id = "roller1";
+
+    let clone = roller.cloneNode(true);
+    clone.id = "roller2";
+    
+    document.querySelector(".wrapper").appendChild(clone);
+    document.querySelector("#roller1").style.left = "0px";
+    document.querySelector("#roller2").style.left = document.querySelector(".year-box").offsetWidth + 'px';
+
+    roller.classList.add('original');
+    roller.classList.add('year-box');
+    clone.classList.add('clone');
+    clone.classList.add('year-box');
+
 
     for (const yearBox of yearBoxes) {
         yearBox.addEventListener('click', zoomIn);
@@ -20,6 +26,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const clickedYearBox = event.currentTarget;
         clickedYearBox.style.transform = "scale(1000)";
         clickedYearBox.style.zIndex = 1;
-        clickedYearBox.style.transition = "ease-in 15s";
+        clickedYearBox.style.transition = "ease-in 18s";
+        clickedYearBox.style.transform = ""
     }
 });
+
